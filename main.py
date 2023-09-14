8903982f99756e66f48a8b0fe18e485557fd0a6b
Nishith Savla <nishithsavla005@gmail.com>
2023-09-15 00:02:21 +0530

    Enhance Conversational Worflow

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+from contextlib import asynccontextmanager
 from http import HTTPStatus
 from pathlib import Path
 from typing import Any
@@ -8,18 +9,33 @@ import uvicorn
 from dotenv import load_dotenv, find_dotenv
 from fastapi import FastAPI, HTTPException, UploadFile
 from fastapi.middleware.cors import CORSMiddleware
-from langchain.document_loaders import YoutubeAudioLoader, DirectoryLoader, UnstructuredPDFLoader, \
-    TextLoader, PyPDFLoader
+from langchain.chains import ConversationalRetrievalChain
+from langchain.chains.base import Chain
+from langchain.chat_models import ChatOpenAI
+from langchain.document_loaders import YoutubeAudioLoader, PyPDFLoader
 from langchain.document_loaders.generic import GenericLoader
 from langchain.document_loaders.parsers import OpenAIWhisperParser
-from langchain.indexes import VectorstoreIndexCreator
+from langchain.embeddings import OpenAIEmbeddings
 from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.vectorstores import Chroma
 
 _ = load_dotenv(find_dotenv())  # read local .env file
-app = FastAPI()
+
+
+@asynccontextmanager
+async def lifespan(_: FastAPI):
+    global vector_db
+    vector_db = Chroma(persist_directory=CHROMA_PERSIST_DIRECTORY,
+                       embedding_function=OpenAIEmbeddings())
+    load_qa()
+    yield
+
+
+app = FastAPI(lifespan=lifespan)
 
 openai.api_key = os.getenv('OPENAI_API_KEY')
 origins = ["*"]
+CHROMA_PERSIST_DIRECTORY = 'docs/chroma/'
 
 app.add_middleware(
     CORSMiddleware,
@@ -29,59 +45,64 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-context = {
-    "role": "system",
-    "content": """
-You are a Technical Query chatbot named `Sahadeva`, an automated service to help users find \
-answers to their queries based on the context provided.
-
-NOTE: Don't include any information out of the given context.
-
-```
-
-You respond in a short, very conversational friendly style.
-""",
-}
+text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size=1500,
+    chunk_overlap=150
+)
+qa: ConversationalRetrievalChain | None = None
+vector_db: Chroma | None = None
 
 
-def get_completion_from_messages(messages: list[dict[str, str]], model: str = "gpt-3.5-turbo",
-                                 temperature: float = 0.3) -> dict[str]:
-    response = openai.ChatCompletion.create(
-        model=model,
-        messages=[context, *messages],
-        temperature=temperature,
+# template = """Use the following pieces of context to answer the question at the end.
+# If you don't know the answer, just say that you don't know, don't try to make up an answer.
+# Use three sentences maximum. Keep the answer as concise as possible.
+# Always say "Thanks for asking!" at the end of the answer.
+#
+# Context: {context}
+# Question: {question}
+# Helpful Answer:"""
+# QA_CHAIN_PROMPT = PromptTemplate(input_variables=["context", "question"], template=template)
+
+def load_qa():
+    global vector_db, qa
+    retriever = vector_db.as_retriever(search_type="mmr", search_kwargs={"top_k": 5})
+    qa = ConversationalRetrievalChain.from_llm(
+        ChatOpenAI(temperature=0),
+        retriever=retriever,
+        # return_source_documents=True,
+        # return_generated_question=True,
     )
-    return response.choices[0].message
 
 
 @app.post("/")
 def root(messages: list[dict[str, str]] = "") -> dict[str, Any]:
+    global qa
     if not messages:
         raise HTTPException(
             status_code=HTTPStatus.BAD_REQUEST,
             detail="Messages are required",
         )
 
-    index = VectorstoreIndexCreator(
-        text_splitter=RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=100)
-    ).from_loaders(
-        [
-            DirectoryLoader("docs/PDFs", loader_cls=PyPDFLoader),
-            # GenericLoader(FileSystemBlobLoader("docs/youtube", glob="*.m4a"),
-            #               OpenAIWhisperParser()),
-            DirectoryLoader("docs/youtube-transcripts", loader_cls=TextLoader),
-        ])
-
-    print("Index Created")
-    return {"message": index.query(messages[-1]['content'])}
+    chat_history = []
+    for i in range(0, len(messages) - 1, 2):
+        chat_history.append((messages[i]['content'], messages[i + 1]['content']))
+    message = qa.run(question=messages[-1]['content'], chat_history=chat_history)
+    messages.append({"role": "assistant", "content": message})
+    return {"messages": messages}
 
 
 @app.post("/upload")
 def upload_documents(files: list[UploadFile]) -> dict[str, str]:
+    docs = []
     for file in files:
-        output_file = Path("docs/PDFs") / file.filename
+        output_file = Path("docs/pdfs") / file.filename
         output_file.parent.mkdir(exist_ok=True, parents=True)
         output_file.write_bytes(file.file.read())
+        docs.extend(PyPDFLoader(output_file.as_posix()).load())
+
+    splits = text_splitter.split_documents(docs)
+    vector_db.add_documents(splits)
+    vector_db.persist()
 
     return {"message": f"Documents uploaded successfully"}
 
@@ -99,13 +120,16 @@ def load_youtube_transcript(url: str) -> dict[str, str]:
         YoutubeAudioLoader([url], youtube_audio_save_dir.as_posix()),
         OpenAIWhisperParser()
     )
-    page_content = loader.load()[0].page_content
-    print(page_content)
+    docs = loader.load()
+    text_splitter.split_documents(docs)
+    vector_db.add_documents(docs)
+    vector_db.persist()
 
-    output_file = Path("docs/youtube-transcripts") / encode_youtube_url(url)
-    output_file.parent.mkdir(exist_ok=True, parents=True)
-    output_file.write_text(page_content)
-
+    # output_file = Path("docs/youtube-transcripts") / encode_youtube_url(url)
+    # output_file.parent.mkdir(exist_ok=True, parents=True)
+    # for doc in docs:
+    #     output_file.write_text(doc.page_content)
+    #
     return {"message": f"Transcript for {url} saved successfully"}
 
 
