eec082e53ec76551c8d8e248a601fa49a0af3671
Nishith Savla <nishithsavla005@gmail.com>
2023-09-18 00:55:44 +0530

    Handle API Rate-limit by setting a backoff

--- helper.py
+++ helper.py
@@ -1,3 +1,5 @@
+import logging
+from time import sleep
 from typing import BinaryIO
 
 import requests
@@ -11,7 +13,17 @@ def extract_text_from_image(file: BinaryIO, filename: str):
                              headers={
                                  'apikey': os.getenv('OCR_SPACE_API_KEY')
                              })
-    return response.json()['ParsedResults'][0]['ParsedText']
+    json = response.json()
+    if isinstance(json, str):
+        logging.info("OCR Space API limit reached, waiting 60 seconds")
+        sleep(60)
+        return extract_text_from_image(file, filename)
+    try:
+        return json['ParsedResults'][0]['ParsedText']
+    except Exception as e:
+        logging.info(json)
+        logging.exception(e)
+        return ""
 
 
 def encode_youtube_url(url) -> str:
